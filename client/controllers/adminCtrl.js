99155ce54c3b71c9f9598970a92b2533547fa2b4
Vipul Sharma <126817942+VipulSharma0001@users.noreply.github.com>
2024-04-10 20:44:08 +0530

    final code
    success

---
+++ client/controllers/adminCtrl.js
@@ -0,0 +1,67 @@
+const userModel = require('../models/userModel');
+const doctorModel = require('../models/doctorModel')
+
+const getAllUserController = async (req, res) => {
+    try {
+        const users = await userModel.find({isDoctor:false});
+        res.status(200).send({
+            success: true,
+            message: 'users data',
+            data: users,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'error while fetching users',
+            error
+        });
+    }
+};
+
+const getAllDoctorController = async (req, res) => {
+    try {
+        const doctors = await doctorModel.find({});
+        res.status(200).send({
+            success: true,
+            message: 'doctors data',
+            data: doctors,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'error while fetching doctors',
+            error
+        });
+    }
+};
+const changeAccountStatusController = async (req, res) => {
+    try {
+        const { doctorId, status } = req.body; // Change 'res' to 'req'
+        const doctor = await doctorModel.findByIdAndUpdate(doctorId, { status });
+        const user = await userModel.findOne({ _id: doctor.userId });
+        const notification = user.notification;
+        notification.push({
+            type: 'doctor-account-request-update',
+            message: `Your doctor account request has been ${status}`,
+            onClickPath: '/notification'
+        });
+        user.isDoctor = status === 'approve' ? true : false; // Corrected assignment
+        await user.save();
+        res.status(201).send({
+            success: true,
+            message: 'Account status updated',
+            data: doctor
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Approval failed",
+            error
+        });
+    }
+};
+
+module.exports = { getAllDoctorController, getAllUserController,changeAccountStatusController };
---
+++ client/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const colors = require('colors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const path = require('path')
+// Config
+require('dotenv').config();
+
+
+//connect db
+
+connectDB();
+
+// Rest object
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(morgan('dev')); // Use 'dev' format for morgan
+
+// Routes
+app.use("/api/v1/user", require("./routes/userRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+//static file
+app.get(rxpress.static(path.join(__dirname,'./client/build')))
+
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
+
+// Port
+const port = process.env.PORT || 8080;
+
+// App listens on PORT
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_MODE} mode on PORT ${process.env.PORT}`.bgCyan.bgWhite);
+});
